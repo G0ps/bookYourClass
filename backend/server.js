5ee39d4377596a51daa6413a775df836c3512c8f
Gopinath <gopinathshanmugavel@gmail.com>
2026-05-22 21:35:52 +0530

    corrected cookies aspect in backend .first commit after realising the seperate of need of front and back git

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,12 @@ const app = express();
 const port = process.env.APPLICATION_PORT
 
 app.use(express.json())
-app.use(cors({origin : true}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser()); // for cookies handling
 
 app.use(mainRouter);
